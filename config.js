51aa032d3206aa2c42f76c5ad4795b2525b2c5f5
Cassiano Danielsson <danielssoncassiano@gmail.com>
2023-09-06 22:27:13 -0300

    Major changes
    Implemented operations for custom modifications of the lists and more complex objects

--- config.js
+++ config.js
@@ -1,57 +1,93 @@
-export const dataConfig = {
-    column: "Name",
-    data: 
-    //all the data go inside this template string
-    `
-    1	Miguel	1	Sophia
-    2	Davi	2	Alice
-    3	Arthur	3	Julia
-    4	Pedro	4	Isabella
-    5   Gabriel	5	Manuela
-    6	Bernardo	6	Laura
-    7	Lucas	7	Luiza
-    8	Matheus	8	Valentina
-    9	Rafael	9	Giovanna
-    10	Heitor	10	Maria Eduarda
-    11	Enzo	11	Helena
-    12	Guilherme	12	Beatriz
-    13	Nicolas	13	Maria Luiza
-    14	Lorenzo	14	Lara
-    15	Gustavo	15	Mariana
-    16	Felipe	16	Nicole
-    17	Samuel	17	Rafaela
-    18	João Pedro	18	Heloísa
-    19	Daniel	19	Isadora
-    20	Vitor	20	Lívia
-    21	Leonardo	21	Maria Clara
-    22	Henrique	22	Ana Clara
-    23	Theo	23	Lorena
-    24	Murilo	24	Gabriela
-    25	Eduardo	25	Yasmin
-    26	Pedro Henrique	26	Isabelly
-    27	Pietro	27	Sarah
-    28	Cauã	28	Ana Julia
-    29	Isaac	29	Letícia
-    30	Caio	30	Ana Luiza
-    31	Vinicius	31	Melissa
-    32	Benjamin	32	Marina
-    33	João	33	Clara
-    34	Lucca	34	Cecília
-    35	João Miguel	35	Esther
-    36	Bryan	36	Emanuelly
-    37	Joaquim	37	Rebeca
-    38	João Vitor	38	Ana Beatriz
-    39	Thiago	39	Lavínia
-    40	Antônio	40	Vitória
-    41	Davi Lucas	41	Bianca
-    42	Francisco	42	Catarina
-    43	Enzo Gabriel	43	Larissa
-    44	Bruno	44	Maria Fernanda
-    45	Emanuel	45	Fernanda
-    46	João Gabriel	46	Amanda
-    47	Ian	47	Alícia
-    48	Davi Luiz	48	Carolina
-    49	Rodrigo	49	Agatha
-    50	Otávio	50	Gabrielly
-    `
+module.exports = config = {
+    column1: {
+        operations: [1, 2, 3],
+        name: "First Name",
+        data:
+            `
+        1	Miguel	1	Sophia
+        2	Davi	2	Alice
+        3	Arthur	3	Julia
+        `
+    },
+    column2: {
+        operations: [1, 3],
+        name: "Second Name",
+        data:
+            `
+        1	Jack	1	Biden
+        2	Rodrigues	2	Moreira
+        3	Santos	3	Silva
+        `
+    },
+
+    column3: {
+        operations: [1, 2, 3],
+        name: "Cities 1",
+        data:
+            `
+        1º	 Joinville	616.323	8,2402
+        2º	 Florianópolis	537.213	7,0386
+        3º	 Blumenau	361.261	4,9931
+        4º	 São José	270.295	3,4572
+        5º	 Itajaí	264.054	3,0881
+        6º	 Chapecó	254.781	3,1013
+        7º	 Palhoça	222.598	2,4348
+        8º	 Criciúma	214.493	2,9896
+        9º	 Jaraguá do Sul	182.660	2,5152
+        10º	 Lages	164.981	2,1416
+        11º	 Brusque	141.385	1,9159
+        12º	 Balneário Camboriú	139.155	2,0335
+        13º	 Tubarão	110.088	1,4600
+        14º	 Camboriú	103.074	1,1880
+        15º	 Navegantes	86.401	1,1683
+        16º	 São Bento do Sul	83.275	1,1762
+        17º	 Concórdia	81.646	1,0313
+        18º	 Biguaçu	76.773	0,9603
+        19º	 Itapema	75.940	0,9447
+        20º	 Caçador	73.720	1,0904
+        21º	 Rio do Sul	72.587	0,9938
+        22º	 Gaspar	72.570	0,9801
+        23º	 Araranguá	71.922	0,9470
+        24º	 Indaial	71.549	0,9858
+        25º	 Içara	59.035	0,7911
+        26º	 Videira	55.466	0,7378
+        27º	 Mafra	55.286	0,7743
+        28º	 Canoinhas	55.016	0,7435
+        29º	 Imbituba	52.581	0,6229
+        30º	 São Francisco do Sul	52.674	0,7461
+        31º	 Xanxerê	51.607	0,7125
+        32º	 Tijucas	51.592	0,5436
+        33º	 Guaramirim	46.711	0,6371
+        34º	 Timbó	46.099	0,6228
+        35º	 Araquari	45.180	0,5572
+        36º	 Barra Velha	45.371	0,4161
+        37º	 São Miguel do Oeste	44.330	0,5621
+        38º	 Laguna	42.785	0,6326
+        39º	 Curitibanos	40.045	0,5456
+        40º	 Rio Negrinho	39.258	0,5816
+        41º	 Campos Novos	36.932	0,5023
+        42º	 Pomerode	34.263	0,4710
+        43º	 Braço do Norte	33.773	0,4673
+        44º	 Penha	33.663	0,4636
+        45º	 Fraiburgo	33.481	0,5004
+        46º	 Porto União	32.970	0,4863
+        47º	 São João Batista	32.687	0,5412
+        48º	 Xaxim	31.918	0,3986
+        49º	 Forquilhinha	31.431	0,3764
+        50º	 Itapoá
+        `
+    },
+
+    column4: {
+        operations: [1],
+        name: "Cities 2",
+        data:
+            `
+        1º	 Joinville	616.323	8,2402
+        2º	 Florianópolis	537.213	7,0386
+        3º	 Blumenau	361.261	4,9931
+        4º	 São José	270.295	3,4572
+        5º	 Itajaí	264.054	3,0881
+        `
+    }
 }
--- index.js
+++ index.js
@@ -1,30 +1,87 @@
-import { dataConfig } from "./config.js"
-import fs from 'fs';
-const dataObj = createObj(dataConfig)
-createFile(dataObj)
-//used in the createObj function:
-function processData(dataConfig){
-    //delete spaces and numbers:
-    const processData = dataConfig.data.replace(/\s+/g, '').replace(/\d/g, "")
-    //split the data and create an array:
-    const arrayData = processData.split(/(?=[A-Z])/)
-    return arrayData
-}
-//used in dataObj const:
-function createObj(dataConfig){
-    const dataObject = {}
-    const arrayData = processData(dataConfig)
-    dataObject.column = dataConfig.column //give an unique value to the obj
-    dataObject["data"] = arrayData //give an array value to the obj
-    return dataObject
-}
-//last function:
-function createFile(dataObj){
-    const processedDataObj = JSON.stringify(dataObj)
-    const dir = './jsons'
-    //create the dir
-    if(!fs.existsSync(dir)){
-        fs.mkdirSync(dir)
+const config = require('./config.js');
+const fs = require('fs');
+
+const DIR_BUILD_NAME = "json"
+iniciate(config)
+
+function iniciate(config) {
+
+    const data_obj_array = []
+    const data_array = processData(config)
+    const config_object_length = Object.values(config).length
+
+
+    for (var i = 0; i < config_object_length; i++) {
+        const data_obj = {
+            name: Object.values(config)[i].name,
+            data: data_array[i]
+        }
+
+        data_obj_array.push(data_obj)
     }
-    fs.writeFile(`./${dir}/${dataObj.column}.json`, processedDataObj, () => { console.log('Json created!' )})
+
+    createFile(data_obj_array)
+
+}
+
+function processData(config) {
+
+    const columns_qty = Object.keys(config).length
+    const columns_data_values = Object.values(config)
+    var array_modifing = []
+    var data_array = []
+
+    for (let i = 0; i < columns_qty; i++) {
+        array_modifing = characters_modifier(columns_data_values, i)
+        data_array.push(array_modifing)
+    }
+
+    //data_array.splice(0, data_array.length - 1)
+
+    return data_array
+
+}
+
+function characters_modifier(obj_data, i) {
+
+    const regex = /[º!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+    if (obj_data[i].operations.includes(1)) {
+        //remove blank spaces
+        obj_data[i].data = obj_data[i].data
+            .replace(/\s+/g, "")
+    }
+
+    if (obj_data[i].operations.includes(2)) {
+        //remove numbers
+        obj_data[i].data = obj_data[i].data
+            .replace(/\d/g, "")
+    }
+
+    if (obj_data[i].operations.includes(3)) {
+        //remove special characters
+        obj_data[i].data = obj_data[i].data
+            .replace(regex, '')
+    }
+
+    //split by upper case  
+    obj_data[i].data = obj_data[i].data
+        .split(/(?=[A-Z])/)
+
+    return obj_data[i].data
+
+}
+
+function createFile(data_obj_array) {
+
+    const processed_json = JSON.stringify(data_obj_array)
+
+    if (!fs.existsSync(`./${DIR_BUILD_NAME}`)) {
+        fs.mkdirSync(`./${DIR_BUILD_NAME}`)
+    }
+
+    fs.writeFile(`./${DIR_BUILD_NAME}/build.json`, processed_json, () => {
+        console.log(`Json created on /${DIR_BUILD_NAME}/build.json`)
+    })
+
 }
